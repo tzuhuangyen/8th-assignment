7acf7fc6e6d98e0d6de74d0b65daa437aac0719b
TzuHuang Yen <yan0912@hotmail.com>
2022-09-14 01:42:02 +0200

    explore

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -2,7 +2,7 @@ $(function () {
   console.log("Hello Bootstrap5");
 });
 
-//click search icon then h1& menu icon d-none then show search form
+//click 搜尋按鈕 search icon then h1& menu icon d-none then show search form
 $(document).ready(function () {
   $(".search-icon").click(function (event) {
     $("h1").toggle();
@@ -11,7 +11,16 @@ $(document).ready(function () {
   });
 });
 
-//click menu icon then menu icon d-none & show close icon
+// click 左側（返回）按鈕icon
+$(document).ready(function () {
+  $(".fa-angle-left").click(function (event) {
+    $("h1").toggle();
+    $(".navbar-toggler-icon").toggle();
+    $(".searchForm").toggle();
+  });
+});
+
+//click 選單 icon then menu icon d-none & show close icon
 $(document).ready(function () {
   $(".navbar-toggler-icon").click(function (event) {
     $(".navbar-toggler-icon").toggle();
@@ -19,7 +28,7 @@ $(document).ready(function () {
   });
 });
 
-// click close-icon then show menu icon
+// click 關閉選單按鈕close-icon then show menu icon
 $(document).ready(function () {
   $(".close-icon").click(function (event) {
     $(".navbar-toggler-icon").toggle();
