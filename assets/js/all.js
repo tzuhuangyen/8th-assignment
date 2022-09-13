5c78889344e9e335910b405d4cfa71ef1bd6fdd2
TzuHuang Yen <yan0912@hotmail.com>
2022-09-13 19:56:26 +0200

    Update 2022-09-13T17:56:23.874Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var _Swiper;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 $(function () {
   console.log("Hello Bootstrap5");
 }); //click search icon then h1& menu icon d-none then show search form
@@ -24,5 +28,18 @@ $(document).ready(function () {
     $(".navbar-toggler-icon").toggle();
     $(".close-icon").toggle();
   });
-});
+}); // artist swiper
+// <!-- Initialize Swiper -->
+
+var swiper = new Swiper(".artistSwiper", (_Swiper = {
+  slidesPerView: "auto",
+  centeredSlides: true
+}, _defineProperty(_Swiper, "slidesPerView", 1), _defineProperty(_Swiper, "spaceBetween", 24), _defineProperty(_Swiper, "pagination", {
+  el: ".swiper-pagination",
+  clickable: true
+}), _defineProperty(_Swiper, "breakpoints", {
+  992: {
+    slidesPerView: 3
+  }
+}), _Swiper));
 //# sourceMappingURL=all.js.map
