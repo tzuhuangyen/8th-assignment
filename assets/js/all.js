bfe8a896fbdbd6bdf70f09795cddb06601191cb1
TzuHuang Yen <yan0912@hotmail.com>
2022-09-14 01:42:30 +0200

    Update 2022-09-13T23:42:28.715Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,7 +6,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
 $(function () {
   console.log("Hello Bootstrap5");
-}); //click search icon then h1& menu icon d-none then show search form
+}); //click 搜尋按鈕 search icon then h1& menu icon d-none then show search form
 
 $(document).ready(function () {
   $(".search-icon").click(function (event) {
@@ -14,14 +14,22 @@ $(document).ready(function () {
     $(".navbar-toggler-icon").toggle();
     $(".searchForm").toggle();
   });
-}); //click menu icon then menu icon d-none & show close icon
+}); // click 左側（返回）按鈕icon
+
+$(document).ready(function () {
+  $(".fa-angle-left").click(function (event) {
+    $("h1").toggle();
+    $(".navbar-toggler-icon").toggle();
+    $(".searchForm").toggle();
+  });
+}); //click 選單 icon then menu icon d-none & show close icon
 
 $(document).ready(function () {
   $(".navbar-toggler-icon").click(function (event) {
     $(".navbar-toggler-icon").toggle();
     $(".close-icon").toggle();
   });
-}); // click close-icon then show menu icon
+}); // click 關閉選單按鈕close-icon then show menu icon
 
 $(document).ready(function () {
   $(".close-icon").click(function (event) {
