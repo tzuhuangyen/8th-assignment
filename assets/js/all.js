287143b7400f66fc529464d986b46c493d8d8daf
TzuHuang Yen <yan0912@hotmail.com>
2022-09-13 15:08:18 +0200

    Update 2022-09-13T13:08:16.496Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,28 @@
 "use strict";
 
 $(function () {
-  console.log('Hello Bootstrap5');
+  console.log("Hello Bootstrap5");
+}); //click search icon then h1& menu icon d-none then show search form
+
+$(document).ready(function () {
+  $(".search-icon").click(function (event) {
+    $("h1").toggle();
+    $(".navbar-toggler-icon").toggle();
+    $(".searchForm").toggle();
+  });
+}); //click menu icon then menu icon d-none & show close icon
+
+$(document).ready(function () {
+  $(".navbar-toggler-icon").click(function (event) {
+    $(".navbar-toggler-icon").toggle();
+    $(".close-icon").toggle();
+  });
+}); // click close-icon then show menu icon
+
+$(document).ready(function () {
+  $(".close-icon").click(function (event) {
+    $(".navbar-toggler-icon").toggle();
+    $(".close-icon").toggle();
+  });
 });
 //# sourceMappingURL=all.js.map
